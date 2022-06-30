9174300ddaf41e7a3476f14fec4d7c3439bdb0fe
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-06-30 22:18:36 +0300

    Initial commit

---
+++ bin/fizz_buzz_variants.dart
@@ -0,0 +1,10 @@
+import 'package:fizz_buzz_variants/fizz_buzz_variants.dart'
+    as fizz_buzz_variants;
+import 'package:fizz_buzz_variants/original_fizz_buzz.dart';
+import 'package:fizz_buzz_variants/switch_wile_fizz_buzz.dart';
+
+void main(List<String> arguments) {
+  fizzBuzz();
+  print("");
+  switchWileFizzBuzz();
+}
---
+++ lib/switch_wile_fizz_buzz.dart
@@ -0,0 +1,15 @@
+void switchWileFizzBuzz() {
+  var command1 = 'FizzBuzz';
+  var command2 = 'Fizz';
+  var command3 = 'Bizz';
+  int i = 0;
+  while (i <= 99) {
+    i++;
+    print(i);
+    switch (command1) {
+      case:
+        i % 3 == 0 || i % 5 == 0;
+        break;
+    }
+  }
+}
