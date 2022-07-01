5a9ef8c8399562e27929b6e2247b9905d25519bd
Georgey Gabaidze <gabaidzegeorgij@gmail.com>
2022-07-01 13:40:21 +0300

    created FizzBuzz with ternary and added his to main function.

---
+++ lib/fizz_buzz_with_ternary.dart
@@ -0,0 +1,15 @@
+void fizzBuzzWithTernary() {
+  for (var i = 1; i <= 100; i++) {
+    var fizzBuzz = i % 15 == 0;
+    var fizz = i % 3 == 0;
+    var buzz = i % 5 == 0;
+
+    fizzBuzz
+        ? print('fizzBuzz')
+        : fizz
+            ? print('fizz')
+            : buzz
+                ? 'Buzz'
+                : print(i);
+  }
+}
